01d839c339f1c96afbb78d9b88b5a94b366d4705
brobohn <brobohn@vt.edu>
2016-04-18 10:45:17 -0400

    Changed GUI. 'Capture' saves photo to gallery.

--- app/src/main/java/com/bactrack/backtrack_mobile/breathezpackage/MainActivity.java
+++ app/src/main/java/com/bactrack/backtrack_mobile/breathezpackage/MainActivity.java
@@ -3,7 +3,11 @@ package com.bactrack.backtrack_mobile.breathezpackage;
 import android.app.Activity;
 import android.content.Intent;
 import android.hardware.Camera;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -25,20 +29,16 @@ import BACtrackAPI.Exceptions.BluetoothNotEnabledException;
 public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
     private static String TAG = "MainActivity";
-
     private TextView statusMessageTextView;
-
     private BACtrackAPI mAPI;
-
     private Button btn_camera;
-
     Camera camera;
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
     Camera.PictureCallback rawCallback;
     Camera.ShutterCallback shutterCallback;
     Camera.PictureCallback jpegCallback;
-    private final String tag = "VideoServer";
+    private final String tag = "Breathe-EZ";
 
     Button start, stop, capture;
 
@@ -46,16 +46,6 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_camera = (Button)findViewById(R.id.camera);
-        btn_camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra("android.intent.extras.CAMERA_FACING", Camera.CameraInfo.CAMERA_FACING_FRONT);
-                startActivity(intent);
-            }
-        });
 
         this.statusMessageTextView = (TextView)this.findViewById(R.id.status_message_text_view_id);
 
@@ -72,14 +62,14 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         }
 
         start = (Button)findViewById(R.id.btn_start);
+        stop = (Button)findViewById(R.id.btn_stop);
+        capture = (Button) findViewById(R.id.capture);
         start.setOnClickListener(new Button.OnClickListener()
         {
             public void onClick(View arg0) {
                 start_camera();
             }
         });
-        stop = (Button)findViewById(R.id.btn_stop);
-        capture = (Button) findViewById(R.id.capture);
         stop.setOnClickListener(new Button.OnClickListener()
         {
             public void onClick(View arg0) {
@@ -87,7 +77,6 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
             }
         });
         capture.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
@@ -99,12 +88,12 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
         rawCallback = new Camera.PictureCallback() {
             public void onPictureTaken(byte[] data, Camera camera) {
                 Log.d("Log", "onPictureTaken - raw");
             }
         };
-
         /** Handles data for jpeg picture */
         shutterCallback = new Camera.ShutterCallback() {
             public void onShutter() {
@@ -114,12 +103,25 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         jpegCallback = new Camera.PictureCallback() {
             public void onPictureTaken(byte[] data, Camera camera) {
                 FileOutputStream outStream = null;
+                String pic_dst = String.format("%s/%d.jpg",
+                        Environment.getExternalStorageDirectory() + "/DCIM/Camera",
+                        System.currentTimeMillis());
+
                 try {
-                    outStream = new FileOutputStream(String.format(
-                            "/sdcard/%d.jpg", System.currentTimeMillis()));
+                    outStream = new FileOutputStream(pic_dst);
                     outStream.write(data);
                     outStream.close();
-                    Log.d("Log", "onPictureTaken - wrote bytes: " + data.length);
+                    Log.d("Log", String.format("onPictureTaken - wrote bytes: %d", data.length));
+                    Log.d("Log", String.format("onPictureTaken - %s", pic_dst));
+
+                    MediaScannerConnection.scanFile(getApplicationContext(), new String[]{pic_dst}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Log.i("ExternalStorage", "Scanned " + path + ":");
+                                    Log.i("ExternalStorage", "-> uri=" + uri);
+                                }
+                            });
+
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -129,6 +131,8 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
                 Log.d("Log", "onPictureTaken - jpeg");
             }
         };
+
+        //start_camera();
     }
 
     public void connectNearestClicked(View v) {
@@ -179,12 +183,65 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         });
     }
 
+
+
+    private void captureImage() {
+        camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+    }
+
+    private void start_camera()
+    {
+        try{
+            Log.i("start_camera", "Starting Camera...");
+            camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
+        }catch(RuntimeException e){
+            Log.e(tag, "init_camera: " + e);
+            return;
+        }
+
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+
+        Camera.Parameters param;
+        param = camera.getParameters();
+        //modify parameter
+        param.setPreviewFrameRate(20);
+        param.setPreviewSize(176, 144);
+        camera.setDisplayOrientation(270);
+        camera.setParameters(param);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+            //camera.takePicture(shutter, raw, jpeg);
+        } catch (Exception e) {
+            Log.e(tag, "init_camera: " + e);
+            return;
+        }
+    }
+
+    private void stop_camera()
+    {
+        camera.stopPreview();
+        camera.release();
+    }
+
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+    }
+
     private final BACtrackAPICallbacks mCallbacks = new BACtrackAPICallbacks() {
         @Override
         public void BACtrackConnected() {
             setStatus(R.string.TEXT_CONNECTED);
         }
-
 
         @Override
         public void BACtrackDidConnect(String s) {
@@ -233,7 +290,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
         @Override
         public void BACtrackResults(float measuredBac) {
-            setStatus(getString(R.string.TEXT_FINISHED) + " " + measuredBac);
+            setStatus(getString(R.string.TEXT_FINISHED) + " " + String.format("%.02f", measuredBac));
         }
 
         @Override
@@ -243,22 +300,18 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
         @Override
         public void a(int i) {
-
         }
 
         @Override
         public void a(byte b) {
-
         }
 
         @Override
         public void b(byte b) {
-
         }
 
         @Override
         public void a(byte[] bytes) {
-
         }
 
         @Override
@@ -272,57 +325,4 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
                 setStatus(R.string.TEXT_ERR_BLOW_ERROR);
         }
     };
-
-    private void captureImage() {
-        // TODO Auto-generated method stub
-        camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-    }
-
-    private void start_camera()
-    {
-        try{
-            camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
-        }catch(RuntimeException e){
-            Log.e(tag, "init_camera: " + e);
-            return;
-        }
-
-        android.hardware.Camera.CameraInfo info =
-                new android.hardware.Camera.CameraInfo();
-
-
-        Camera.Parameters param;
-        param = camera.getParameters();
-        //modify parameter
-        param.setPreviewFrameRate(20);
-        param.setPreviewSize(176, 144);
-        camera.setDisplayOrientation(270);
-        camera.setParameters(param);
-        try {
-            camera.setPreviewDisplay(surfaceHolder);
-            camera.startPreview();
-            //camera.takePicture(shutter, raw, jpeg)
-        } catch (Exception e) {
-            Log.e(tag, "init_camera: " + e);
-            return;
-        }
-    }
-
-    private void stop_camera()
-    {
-        camera.stopPreview();
-        camera.release();
-    }
-
-    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-        // TODO Auto-generated method stub
-    }
-
-    public void surfaceCreated(SurfaceHolder holder) {
-        // TODO Auto-generated method stub
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // TODO Auto-generated method stub
-    }
 }
